affa9703761f9736fe45a135a1124e0bba2ea160
margareta bunga kelore <margaretabungakelore@gmail.com>
2025-03-22 14:35:15 +0800

    Keterangan commit

---
+++ pertemuan302/src/pertemuan302/Pertemuan302.java
@@ -0,0 +1,45 @@
+
+package pertemuan302;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author maragerata bunga kelore
+ * TGL 22 MARET 2025
+ */
+public class Pertemuan302 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      //Nilai dinyatakan lulus apabila 75 ke atas
+      int nilai = 0;
+      String nama, nim;
+      String ket="";
+      
+        Scanner dtIN = new Scanner(System.in);
+        
+        System.out.print("Input Nama Mahasiswa: ");
+      nama = dtIN.nextLine();
+      
+      System.out.print("Input nim: ");
+      nim = dtIN.nextLine();
+      
+      System.out.print("Input Nilai: ");
+      nilai = Integer.parseInt(dtIN.nextLine() );
+      
+      
+      
+      
+      if(nilai >=75) {
+          ket ="Lulus";
+      }else{
+          ket ="Tidak Lulus";
+      }
+      
+      System.out.printf ("mahasiswa dengan nama %s (%s) adalah %d, dinyatakan %s", nama,nim,nilai,ket);
+    }
+    
+}
